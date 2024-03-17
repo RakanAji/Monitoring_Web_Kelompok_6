15d7f3d4aabd428d40d0f458d22377de6ed497e1
rakanaji22 <rakanaji@student.telkomuniversity.ac.id>
2024-03-18 01:15:50 +0700

    WEB RESPONSIVE

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,22 +1,23 @@
-import { Navbar, Nav } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap";
 import React from 'react';
 import "../css/main.css";
 
-
 const Navigationbar = () => {
   return (
-    <div>
-      <Navbar  className="navigation-bar"> 
-        <Navbar.Brand href="/" style={{marginRight: '150px'}}>SMART PLANT</Navbar.Brand>
-        <Nav >
-          
-          <Nav.Link href="#deskripsialat" style={{fontSize: 25, marginRight: '150px'}}>Deskripsi Alat </Nav.Link> 
-          <Nav.Link href="#datamonitor" style={{fontSize: 25, marginRight: '150px'}}>Data Monitoring Alat</Nav.Link>
-          <Nav.Link href="#profile" style={{fontSize: 25}}>Profile Tim</Nav.Link>
-        </Nav>
+    <div >
+      <Navbar className="navigation-bar" expand="lg">
+        <Navbar.Brand href="/" className="mr-auto" >SMART PLANT</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#deskripsialat"className="mr-auto" >Deskripsi Alat</Nav.Link>
+            <Nav.Link href="#datamonitor" className="mr-auto">Data Monitoring Alat</Nav.Link>
+            <Nav.Link href="#profile" className="mr-auto">Profile Tim</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
 };
 
-export default Navigationbar
+export default Navigationbar;
--- src/components/ProfileTim.js
+++ src/components/ProfileTim.js
@@ -3,18 +3,19 @@ import { Card, Row, Col } from 'react-bootstrap';
 import aji from '../assets/img/foto_aji.jpg';
 import alip from '../foto_alip.jpg';
 import rehan from '../assets/img/rehan.jpg';
+import regi from '../assets/img/regi.jpg';
 
 function ProfileTim() {
 
   const pembuatData = [
     { nama: 'Rayhan Syailendra', nim: '1101220096', asal: 'Jakarta', jurusan: 'Teknik Telekomunikasi', foto:rehan },
     { nama: 'Rakan Aji Pratama', nim: '1101213030', asal: 'Bogor', jurusan: 'Teknik Telekomunikasi', foto:aji },
-    { nama: 'Regina Ramadhani', nim: '1101223238', asal: 'Bekasi', jurusan: 'Teknik Telekomunikasi' },
+    { nama: 'Regina Ramadhani', nim: '1101223238', asal: 'Bekasi', jurusan: 'Teknik Telekomunikasi', foto:regi},
     { nama: 'Yassar Ahmad Alif', nim: '1101223382', asal: 'Jawa', jurusan: 'Teknik Telekomunikasi', foto:alip },
   ];
 
   return (
-    <div id='profile'>
+    <div id='profile' className='container'>
     <Row>
       <Col sm={12}>
         <Card>
