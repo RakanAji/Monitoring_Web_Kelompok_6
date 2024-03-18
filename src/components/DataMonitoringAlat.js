10ec37f56a5c77d2b92e0a9540c396373bdef660
rakanaji22 <rakanaji@student.telkomuniversity.ac.id>
2024-03-18 13:02:59 +0700

    fix responsive

--- src/components/DataMonitoringAlat.js
+++ src/components/DataMonitoringAlat.js
@@ -57,11 +57,7 @@ const DataMonitoringAlat = () => {
   }, []);
 
   return (
-<<<<<<< Updated upstream
     <div id='datamonitor' className='container' style={{ padding: '20px', marginTop:'150px', marginBottom:'400px' }}>
-=======
-    <div id='datamonitor' className='container' style={{ padding: '20px' }}>
->>>>>>> Stashed changes
       <Card>
         <Card.Body >
           <Card.Title style={{ fontSize: '24px', marginBottom: '20px' }}>Data Monitoring Alat</Card.Title>
